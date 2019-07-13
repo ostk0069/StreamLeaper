6310c695e0213716ed8522371407580e94080d1e
Takuma Osada <judgment9ttk069@gmail.com>
2019-07-13 23:23:28 +0900

    add sample vc

---
+++ Sources/StreamLeaper/LeaperBaseViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LeaperBaseViewController.swift
+//  StreamLeaper iOS
+//
+//  Created by Takuma Osada on 2019/07/13.
+//
+
+import Foundation
+import UIKit
+
+open class LeaperBaseViewController: UIViewController {
+    
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+}
